ff9f96d3e93d274e56c3a58e5553cf213ee1ed86
zhanghuiyong <zhanghuiyong@startdt.com>
2022-03-25 15:40:06 +0800

    nacos config test

--- test-nacos/src/main/java/com/test/nacos/NacosApplication.java
+++ test-nacos/src/main/java/com/test/nacos/NacosApplication.java
@@ -1,6 +1,7 @@
 package com.test.nacos;
 
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySources;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: zhanghuiyong
  * @create: 2022-03-22 20:35
  */
-@SpringBootApplication
-@NacosPropertySource(dataId = "${spring.application.name}", autoRefreshed = true)
+@SpringBootApplication(scanBasePackages = {"com.test.nacos"})
+@NacosPropertySources(value = {@NacosPropertySource(dataId = "nuza-common", autoRefreshed = true),
+        @NacosPropertySource(dataId = "${spring.application.name}", autoRefreshed = true)
+})
 public class NacosApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosApplication.class, args);
